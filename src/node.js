a2d9db3e79d755943e16e62f3a4761fedb88b919
annalikhuta <annalikhuta@gmail.com>
2019-03-21 13:47:24 +0300

    let's try doit!

--- src/node.js
+++ src/node.js
@@ -1,5 +1,11 @@
 class Node {
 	constructor(data, priority) {
+    this.data=data;
+    this.priority=priority;
+    this.next=null;
+    this.parent=null;
+    this.left=null;
+    this.right=null;
 
 	}
 
