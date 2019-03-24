12f5aa9989d5cedeabce83ea3bb8c7cbb801345b
annalikhuta <annalikhuta@gmail.com>
2019-03-24 20:46:55 +0300

    +more

--- src/max-heap.js
+++ src/max-heap.js
@@ -17,18 +17,39 @@ class MaxHeap {
 
     }
     if (this.root!=null){
-
+      this.detachRoot();
     }
 
 		
 	}
 
 	detachRoot() {
-		
+    if(this.root.left==null ){
+      // console.log('here no have child')
+      let copyRoot=this.root;
+      this.root=null;
+      return copyRoot
+    }
+
+    if(this.root.left){
+      // console.log('here  have child')
+
+      let arrAfterDetach=this.parentNodes.filter(item=>{
+        return item!=this.root;
+      })
+      this.parentNodes=arrAfterDetach;
+
+      return this.root
+    }
+     
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+		let lastInsertNode=this.parentNodes[this.parentNodes.length-1];
+    // console.log(lastInsertNode)
+    this.root.priority=lastInsertNode.priority;
+    this.root.data=lastInsertNode.data;
+    lastInsertNode.parent.removeChild(lastInsertNode)
 	}
 
 	size() {
@@ -36,8 +57,13 @@ class MaxHeap {
 	}
 
 	isEmpty() {
-		
-	}
+		if(this.root==null){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 
 	clear() {
 		this.root=null;
